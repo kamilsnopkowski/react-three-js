c4f82f628be6531da1595655eb03b3c585d4885d
Kamil <k.snopkowski@pleodigital.com>
2022-11-10 10:55:01 +0100

    adding textures and background map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import { Canvas, useFrame, useThree, extend } from '@react-three/fiber'
-import { useRef } from 'react'
+import { Canvas, useFrame, useThree, useLoader, extend } from '@react-three/fiber'
+import { useTexture } from "@react-three/drei"
+import { useRef, Suspense } from 'react'
 import { OrbitControls } from "three/addons/controls/OrbitControls";
 import * as THREE from 'three';
 import { useHelper } from '@react-three/drei/native'
+import WoodTexture from './assets/images/wood.jpg'
+import ShopTexture from './assets/images/autoshop.jpg'
 extend({ OrbitControls })
 
 const PointLight = props => {
     const light = useRef();
-    //remember to pass radius and color
     useHelper(light, THREE.PointLightHelper,0.5,'cyan');
     return(
             <pointLight
@@ -16,16 +18,28 @@ const PointLight = props => {
                 color={'#fff'}
                 intensity={1}
                 distance={10}
-                position={[3,3,3]}
+                position={[3,5,3]}
                 castShadow
                 shadow-mapSize-height={512}
                 shadow-mapSize-width={512}
             />
     )
 }
+
+const Background = props => {
+    const texture = useTexture(ShopTexture)
+    //grabbing renderer
+    const { gl } = useThree()
+    const formatted = new THREE.WebGLCubeRenderTarget(texture.image.height).fromEquirectangularTexture(gl, texture)
+    //in course dude passed formatted as object to the primitive and it worked for him
+    //but in my case I needed to pass exactly the texture from the formatted variable
+    return (
+        <primitive attach='background' object={formatted.texture}/>
+    )
+}
+
 const Orbit = () => {
     const { camera, gl } = useThree();
-    //useThree works similar way to useFrame but it runs only once
     return (
         <orbitControls args={[camera, gl.domElement ]}/>
     )
@@ -33,17 +47,15 @@ const Orbit = () => {
 
 const Box = (props) => {
     const ref = useRef(null);
+    const texture = useLoader(THREE.TextureLoader, WoodTexture)
     useFrame((state)=>{
         ref.current.rotation.x+= 0.01;
         ref.current.rotation.y+= 0.01;
     })
     return (
         <mesh ref={ref} {...props} castShadow  >
-            <boxGeometry/>
-            {/* main params for materials: opacity={1} transparent wireframe metalness={1} roughness={0} */}
-            {/*setting transparent, clearcoat=1 and transmission=1 on meshPhysicalMaterial */}
-            {/*makes it see through while still being light reflectable */}
-            <meshPhysicalMaterial color="pink" transparent roughness={0} clearcoat={1} transmission={0.5} reflectivity={1} side={THREE.DoubleSide}/>
+            <sphereGeometry args={[1,100,100]}/>
+            <meshPhysicalMaterial map={texture}/>
         </mesh>
     )
 }
@@ -60,19 +72,22 @@ const Floor = (props) => {
 function App() {
   return (
       <div className="webgl">
-          <Canvas
-              style={{ backgroundColor: 'black'}}
-              camera={{ position: [3,3,3]}}
-              shadows={true}
-          >
-              <fog attach='fog' color={'black'} near={1} far={10}/>
-              <Box position={[3,1,3]} />
-              <Floor  position={[0,-0.05,0]} />
-              <ambientLight intensity={0.3} color={'red'}/>
-              <axesHelper args={[5]}/>
-              <PointLight/>
-              <Orbit/>
-          </Canvas>
+          {/*Suspense forces all children to wait with render for async actions to happen*/}
+          <Suspense fallback={null}>
+              <Canvas
+                  style={{ backgroundColor: 'black'}}
+                  camera={{ position: [5,3,5]}}
+                  shadows={true}
+              >
+                  <Background/>
+                  <Box position={[3,1,3]} />
+                  <Floor  position={[0,-0.05,0]} />
+                  <ambientLight intensity={0.3} color={'white'}/>
+                  <axesHelper args={[5]}/>
+                  <PointLight/>
+                  <Orbit/>
+              </Canvas>
+          </Suspense>
       </div>
   );
 }
