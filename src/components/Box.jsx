f4360a87bb4bf66aadfb88856413b0ce68644aa6
Kamil <k.snopkowski@pleodigital.com>
2022-11-15 09:08:31 +0100

    removed scaling and added physics

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,38 +1,16 @@
-import { useRef } from 'react'
-import {useFrame, useLoader} from "@react-three/fiber";
+import { useLoader} from "@react-three/fiber";
 import * as THREE from "three";
 import WoodTexture from "../assets/images/wood.jpg";
-import {scale} from '../utils/utils';
-
-const handlePointerDown = event => {
-    event.object.active = true;
-    if(window.activeMesh) {
-        scale(window.activeMesh, 1,1,1)
-        window.activeMesh.active = false;
-    }
-    window.activeMesh = event.object;
-}
-
-const handlePointerEnter = event => scale(event.object, 2,2,2);
-const handlePointerLeave = event => {
-    if(event.object.active) return;
-    scale(event.object, 1,1,1);
-}
+import { useBox } from '@react-three/cannon'
 
 const Box = (props) => {
-    const ref = useRef(null);
+    const [ref, api] = useBox(()=>({mass: 1, ...props}));
     const texture = useLoader(THREE.TextureLoader, WoodTexture)
-    useFrame((state)=>{
-        ref.current.rotation.x+= 0.01;
-        ref.current.rotation.y+= 0.01;
-    })
 
     return (
         <mesh ref={ref}
+              api={api}
               castShadow
-              onPointerDown={handlePointerDown}
-              onPointerEnter={handlePointerEnter}
-              onPointerLeave={handlePointerLeave}
               {...props}
         >
             <boxGeometry args={[1,1,1]}/>
