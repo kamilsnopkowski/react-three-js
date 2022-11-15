c7a2afca992875d1ea92b2bf50482f2d58fc0028
Kamil <k.snopkowski@pleodigital.com>
2022-11-15 09:13:03 +0100

    fixed orbitControls

--- src/components/Orbit.jsx
+++ src/components/Orbit.jsx
@@ -1,10 +1,12 @@
 import {useThree} from "@react-three/fiber";
 import { extend } from '@react-three/fiber'
 import { OrbitControls } from "three/addons/controls/OrbitControls";
+import {useEffect} from "react";
 extend({ OrbitControls })
 
 const Orbit = () => {
-    const { camera, gl } = useThree();
+    const { camera, gl,scene } = useThree();
+    useEffect(() => scene.orbitControls.enabled = true,[])
     return (
         <orbitControls args={[camera, gl.domElement ]} attach="orbitControls" enabled={false}/>
     )
